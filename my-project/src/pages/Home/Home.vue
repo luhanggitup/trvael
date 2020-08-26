dc2f195175efebacd34b9f74c448ddde71da0030
unknown <782690035@qq.com>
2020-08-27 02:40:37 +0800

    change

--- my-project/src/pages/Home/Home.vue
+++ my-project/src/pages/Home/Home.vue
@@ -1,15 +1,18 @@
 <template>
   <div>
-   <home-header>这是HOME组件</home-header>
+   <home-header></home-header>
+   <home-swiper></home-swiper>
   </div> 
 </template>
 
 <script>
 import HomeHeader from './components/Header'
+import HomeSwiper from './components/swiper'
 export default {
 name:'Home',
 components : {
-	HomeHeader
+  HomeHeader,
+  HomeSwiper
 }
 }
 </script>
---
+++ my-project/src/pages/Home/components/swiper.vue
@@ -0,0 +1,49 @@
+<template>
+  <div class="wrapper">
+    <swiper :options="swiperOption">
+      <swiper-slide v-for="item of swiperList" :key='item.id'>
+        <img class='swiper-img' :src="item.imgUrl">
+      </swiper-slide>
+      <div class="swiper-pagination" slot="pagination"></div>
+    </swiper>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "HomeSwiper",
+  data() {
+    return {
+      swiperOption: {
+        pagination:".swiper-pagination",
+        loop:true
+      },
+      swiperList:[
+        {
+          id:'001',
+          imgUrl:'https://imgs.qunarzz.com/vs_ceph_vcimg/7c151dc2ebcab3c802569ef285aa634f.jpeg'
+        },
+        {
+          id:'002',
+          imgUrl:'https://imgs.qunarzz.com/vc/57/28/30/a8bc10efbf8d119be4ea526ea6.jpg_92.jpg'
+        }
+      ]
+    };
+  },
+};
+</script>
+
+<style>
+.swiper-pagination-bullet-active{
+  background:white;
+}
+.wrapper {
+  overflow: hidden;
+  width: 100%;
+  height: 31.25%;
+  background:red;
+}
+.swiper-img {
+  width: 100%;
+}
+</style>
