4d5cb6bdd17b563be4ca418f8a4252db67f8bb59
unknown <782690035@qq.com>
2020-08-29 20:59:05 +0800

    weekend

---
+++ my-project/src/pages/Home/components/weekend.vue
@@ -0,0 +1,77 @@
+<template>
+  <div>
+    <div class="title">周末去哪儿</div>
+    <ul>
+      <li class="item border-bottom" v-for="item in recommendList" :key="item.id">
+          <div class="item-img-wrapper">
+            <img class='item-img' :src="item.imgUrl">
+          </div>
+        <div class="item-info">
+          <p class="item-title">{{item.title}}</p>
+          <p class="item-desc">{{item.desc}}</p>
+        </div>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+name:'HomeWeekend',
+data(){
+  return{
+    recommendList :[
+      {
+        id:'001',
+        imgUrl:'http://img1.qunarzz.com/travel/d3/1807/82/f587e895142f80b5.jpg?ver=1',
+        title:'大连圣亚海洋世界大',
+        desc:'浪费大连首站，浪漫的海洋主题公园大连圣亚海洋世界'
+      },
+       {
+        id:'002',
+        imgUrl:'http://img1.qunarzz.com/travel/d3/1807/82/f587e895142f80b5.jpg?ver=1',
+        title:'大连圣亚海洋世界大',
+        desc:'浪费大连首站，浪漫的海洋主题公园大连圣亚海洋世界'
+      },
+       {
+        id:'003',
+        imgUrl:'http://img1.qunarzz.com/travel/d3/1807/82/f587e895142f80b5.jpg?ver=1',
+        title:'大连圣亚海洋世界大',
+        desc:'浪费大连首站，浪漫的海洋主题公园大连圣亚海洋世界'
+      }
+    ]
+  }
+}
+}
+</script>
+
+<style lang='stylus' scoped>
+@import "~styles/mixins"
+.title{
+  margin-top .2rem
+  line-height .6rem
+  background #eee
+  text-indent .2rem
+}
+.item-img-wrapper{
+  height 0
+  overflow hidden
+  padding-bottom 33.9%
+}
+.item-img{
+  width 100%
+}
+.item-info{
+  padding .1rem
+}
+.item-title{
+  line-height .54rem
+  font-size .32rem
+  ellipsis()
+}
+.item-desc{
+  line-height .4rem
+  color #ccc
+  ellipsis()
+}
+</style>
