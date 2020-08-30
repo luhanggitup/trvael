3b36b33a442124d561fcdba9855582c1bfdfd90e
unknown <782690035@qq.com>
2020-08-30 12:00:16 +0800

    city-search

---
+++ my-project/src/pages/city/components/Search.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="search">
+    <input type="text" placeholder="输入城市名或拼音">
+  </div>
+</template>
+
+<script>
+export default {
+  name:'CitySearch'
+}
+</script>
+
+<style lang='stylus' scoped>
+@import "~styles/varilbles"
+.search{
+    height .72rem
+    padding  0 .1rem 
+    background $bgcolor
+}
+.search input{
+  width 100%
+  box-sizing border-box
+  padding 0 .1rem 
+  height .62rem
+  line-height .62rem
+  text-align center
+  border-radius .06rem
+  color #666
+}
+</style>
