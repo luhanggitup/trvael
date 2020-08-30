568725dc2ee52f4bba290940d1be7803923ebe3e
unknown <782690035@qq.com>
2020-08-30 20:55:35 +0800

    city-list

---
+++ my-project/src/pages/city/components/Alphabet.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="list">
+    <li class="item">A</li>
+    <li class="item">A</li>
+    <li class="item">A</li>
+    <li class="item">A</li>
+    <li class="item">A</li>
+  </div>
+</template>
+
+<script>
+export default {
+  name:'CityAlphabet'
+}
+</script>
+
+<style lang="stylus" scoped>
+@import '~styles/varilbles'
+.list{
+  display flex
+  flex-direction column
+  justify-content center
+  list-style none
+  position absolute 
+  top 1.58rem
+  right 0
+  bottom 0
+  width .4rem
+}
+.item{
+  text-align center
+  line-height .4rem
+  color $bgcolor
+}
+
+</style>
