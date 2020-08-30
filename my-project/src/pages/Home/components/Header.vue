fb8dce91bc213da245b8861bac6b3a8fa5f2620d
unknown <782690035@qq.com>
2020-08-30 11:11:27 +0800

    city-router

--- my-project/src/pages/Home/components/Header.vue
+++ my-project/src/pages/Home/components/Header.vue
@@ -1,68 +1,76 @@
 <template>
   <div class="header">
     <div class="header-left">
-		<div class="iconfont back-icon">&#xe60a;</div>
-		</div>
+      <div class="iconfont back-icon">&#xe60a;</div>
+    </div>
     <div class="header-input">
-			<span class="iconfont">&#xe627;</span>
-			输入城市/景点	/游玩
-			</div>
-    <div class="header-right">
-			{{this.city}}
-		<span class="iconfont .arrow-icon">&#xe64a;</span>
-		</div>
+      <span class="iconfont">&#xe627;</span>
+      输入城市/景点 /游玩
+    </div>
+    <router-link to="/city">
+      <div class="header-right">
+        {{this.city}}
+        <span class="iconfont .arrow-icon">&#xe64a;</span>
+      </div>
+    </router-link>
   </div>
 </template>
 
 <script>
 export default {
-	name: "HomeHeader",
-	props : {
-		city : String
-	}
+  name: "HomeHeader",
+  props: {
+    city: String,
+  },
 };
 </script>
 
 <style lang='stylus' scoped>
 // 整体布局
 @import '~styles/varilbles';
+
 .header {
   display: flex;
-	background:$bgcolor;
-	line-height .86rem
-	height:.86rem;
-	line-height: .86rem;
-	color:#fff
+  background: $bgcolor;
+  line-height: 0.86rem;
+  height: 0.86rem;
+  line-height: 0.86rem;
+  color: #fff;
 }
+
 .header-left {
   width: 0.64rem;
   float: left;
 }
-.back-icon{
-	text-align:center;
-	font-size:.4rem;
+
+.back-icon {
+  text-align: center;
+  font-size: 0.4rem;
 }
 
 // 内容部分
 .header-input {
   flex: 1;
-	height:.64rem;
-	line-height:.64rem;
-	margin-top:.12rem;
-	padding-left:.2rem;
-	margin-left:.2rem;
-	background:#fff;
-	border-radius:.1rem;
-	color:#ccc;
+  height: 0.64rem;
+  line-height: 0.64rem;
+  margin-top: 0.12rem;
+  padding-left: 0.2rem;
+  margin-left: 0.2rem;
+  background: #fff;
+  border-radius: 0.1rem;
+  color: #ccc;
 }
+
 .header-right {
   width: 1.24rem;
   float: left;
-	text-align:center;
+  text-align: center;
+	color #fff
 }
- .arrow-icon{
-	margin-left -.14rem
-	font-size:.24rem;
-	background red
+
+.arrow-icon {
+  margin-left: -0.14rem;
+  font-size: 0.24rem;
+  background: red;
 }
 </style>
---
+++ my-project/src/pages/city/components/Header.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class="header">
+    城市选择
+      <router-link to="/">
+         <div class="iconfont header-back">&#xe60a;</div>
+      </router-link>
+  </div>
+</template>
+
+<script>
+export default {
+  name:'CityHeader'
+
+}
+</script>
+
+<style lang='stylus' scoped>
+@import '~styles/varilbles'
+.header{
+  position relative
+  overflow hidden
+  height .86rem
+  line-height .86rem
+  text-align center
+  color #fff
+  background $bgcolor
+  font-size .32rem 
+}
+.header-back{
+  position absolute
+  width .64rem
+  top 0px
+  left 0px
+  text-align center
+  font-size .4rem
+  color #ffffff
+}
+
+</style>
