81a9543716ba9e47eaeb463a5107963cd0c9294c
unknown <782690035@qq.com>
2020-09-06 21:17:35 +0800

    finish pages

--- my-project/src/App.vue
+++ my-project/src/App.vue
@@ -1,6 +1,12 @@
 <template>
   <div id="app">
-    <router-view/>
+    <!-- keep-aline标签是vue自带的一个标签，解决
+    每次页面请求的时候，ajax都去请求city。json数据
+    多次请求，性能过低 -->
+
+    <keep-alive>
+       <router-view/>
+    </keep-alive> 
   </div>
 </template>
 
--- my-project/src/pages/Home/Home.vue
+++ my-project/src/pages/Home/Home.vue
@@ -9,12 +9,14 @@
 </template>
 
 <script>
-import HomeHeader from "./components/Header";
-import HomeSwiper from "./components/swiper";
-import HomeIcons from "./components/icons";
-import HomeRecommend from "./components/Recommend";
-import HomeWeekend from "./components/weekend";
-import axios from "axios";
+import HomeHeader from "./components/Header"
+import HomeSwiper from "./components/swiper"
+import HomeIcons from "./components/icons"
+import HomeRecommend from "./components/Recommend"
+import HomeWeekend from "./components/weekend"
+import axios from "axios"
+import {mapState} from "vuex"
+
 export default {
   name: "Home",
   components: {
@@ -26,21 +28,25 @@ export default {
   },
   data() {
     return {
+      lastCity:'',
       swiperList: [],
       iconList: [],
       recommendList: [],
       weekendList: [],
     };
   },
+  computed:{
+    ...mapState(['city'])
+  },
   methods: {
     getHomeInfo() {
-      axios.get("/api/index.json").then(this.getHomeInfoSucc);
+      axios.get("/api/index.json?city=" + this.city)
+      .then(this.getHomeInfoSucc);
     },
     getHomeInfoSucc(res) {
       res = res.data;
       if (res.ret && res.data) {
-        const data = res.data;
-      
+        const data = res.data;     
         this.swiperList = data.swiperList;
         this.iconList = data.iconList;
         this.recommendList = data.recommendList;
@@ -49,9 +55,20 @@ export default {
     },
   },
   // 生命周期函数
+  // 页面挂载
   mounted() {
+    this.lastCity = this.city
     this.getHomeInfo();
   },
+  // keep-alive方式里面的生命周期函数
+  // 当页面重新执行的时候运行
+  activated(){
+    if ( this.lastCity !== this.city) {
+      // 这次的页面和上次的不相同的时候，重新发送ajax请求
+      this.lastCity = this.city
+      this.getHomeInfo();
+    }  
+  }
 };
 </script>
 
